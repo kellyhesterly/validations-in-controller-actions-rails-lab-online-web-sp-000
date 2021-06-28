06af7974676889a1e05519692aee582ad7ed735b
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-28 21:17:21 +0000

    Automatically backed up by Learn

--- app/validators/email_validator.rb
+++
@@ -1,7 +0,0 @@
-class EmailValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.email.uniqueness == false
-      record.errors[:email] << "Name can't be blank, Email has already been taken"
-    end
-  end
-end
