4b9055367a346dd3896c294b1ac55a1d9a43d6f2
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-28 21:34:08 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :post, presence: true
 
   def post
-    @post = Post.each {|post| post.id == post.id}
+    @post = Post.akk {|post| post.id == post.id}
   end
 
 end
