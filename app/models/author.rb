82560ff33dc059102f6088e494d96f71b265b4ba
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-28 21:15:20 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ActiveRecord::Base
   include ActiveModel::validations
   validates_with EmailValidator
+
   validates :name, presence: true
   validates :email, uniqueness: true
 end
